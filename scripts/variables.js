927c353ef9e864db01e5394edc86813b1a3b955d
colesmith001 <cole.smith@sdgku.edu>
2026-05-16 11:54:19 -0700

    variables

---
+++ scripts/variables.js
@@ -0,0 +1,54 @@
+console.log("=======")
+console.log("Variables script");
+console.log("=======")
+
+// string text
+let firstName = "cole";
+// numerical 34,67,3.0
+let age= "99";
+
+
+// boolean
+let isStudent = false;
+let isProfessor = true;
+
+console.log(firstName);
+console.log(age);
+console.log(isStudent);
+
+// arithmetic operations
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+let sub = num1 - num2;
+let div = num1 / num2;
+let mult = num1 * num2;
+
+
+// string.interpolation
+console.log(`${num1} + ${num2} = ${sum}`)
+console.log(`${num1} - ${num2} = ${sub}`)
+console.log(`${num1} / ${num2} = ${div}`)
+console.log(`${num1} * ${num2} = ${mult}`)
+
+let aVariable;
+aVariable = 40
+
+// mini challenge
+let lastName = "smith";
+// Create a variable for your last name.
+let height = "6ft 2in"
+// Create a variable for your height.
+
+// Finally, show each variable separately in the console.
+console.log(height);
+console.log(lastName);
+
+// challenge circle
+let r = 3**2
+let pie = 3.14
+let circle = pie * r
+
+
+console.log(`${pie} * ${r} = ${circle}`)
+
